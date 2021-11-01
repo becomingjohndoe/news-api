cd4b3a7cf9f0a46000e680730e60300b7f74889b
becomingjohndoe <karl.swatman@gmail.com>
2021-11-01 11:11:25 +0000

    created tables in seed.js

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,44 @@
-const seed = (data) => {
-  const { articleData, commentData, topicData, userData } = data;
-  // 1. create tables
-  // 2. insert data
+const db = require("../connection");
+const format = require("pg-format");
+
+const seed = async (data) => {
+	const { articleData, commentData, topicData, userData } = data;
+	await db.query(`DROP TABLE IF EXISTS comments;`);
+	await db.query(`DROP TABLE IF EXISTS articles;`);
+	await db.query(`DROP TABLE IF EXISTS users;`);
+	await db.query(`DROP TABLE IF EXISTS topics;`);
+	await db.query(
+		`CREATE TABLE topics (
+      slug SERIAL PRIMARY KEY,
+      description VARCHAR
+    );
+    `
+	);
+	await db.query(`CREATE TABLE users(
+    username SERIAL PRIMARY KEY,
+    avatar_url VARCHAR,
+    name VARCHAR
+  );`);
+	await db.query(`CREATE TABLE articles (
+    article_id SERIAL PRIMARY KEY,
+    title VARCHAR,
+    body VARCHAR,
+    votes INT,
+    topic INT REFERENCES topics(slug),
+    author INT REFERENCES users(username),
+    created_at VARCHAR
+  );`);
+	await db.query(`CREATE TABLE comments (
+    comment_id SERIAL PRIMARY KEY,
+    author INT REFERENCES users(username),
+    article_id INT REFERENCES articles(article_id),
+    votes INT,
+    created_at INT,
+    body VARCHAR
+  );`);
+	// 1. create tables
+
+	// 2. insert data
 };
 
 module.exports = seed;
