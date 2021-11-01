f45c2fe5a9cf592e04ba6692874b38c9b41177cf
becomingjohndoe <karl.swatman@gmail.com>
2021-11-01 15:50:50 +0000

    removes console logs from articles controller

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -3,7 +3,6 @@ const { selectArticleById } = require("../models/articles.model");
 exports.getArticlesById = async (req, res, next) => {
 	try {
 		const article = await selectArticleById(req.params);
-		console.log({ article });
 		res.status(200).send({ article });
 	} catch (err) {
 		next(err);
