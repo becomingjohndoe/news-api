d84c70e481e718dc804072beca5c7098174f49bb
becomingjohndoe <karl.swatman@gmail.com>
2021-11-01 11:23:00 +0000

    added current timestamp on comments and articles tables

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -3,19 +3,21 @@ const format = require("pg-format");
 
 const seed = async (data) => {
 	const { articleData, commentData, topicData, userData } = data;
+	// drop tables
 	await db.query(`DROP TABLE IF EXISTS comments;`);
 	await db.query(`DROP TABLE IF EXISTS articles;`);
 	await db.query(`DROP TABLE IF EXISTS users;`);
 	await db.query(`DROP TABLE IF EXISTS topics;`);
+	// create tables
 	await db.query(
 		`CREATE TABLE topics (
-      slug SERIAL PRIMARY KEY,
+      slug VARCHAR PRIMARY KEY,
       description VARCHAR
     );
     `
 	);
 	await db.query(`CREATE TABLE users(
-    username SERIAL PRIMARY KEY,
+    username VARCHAR PRIMARY KEY,
     avatar_url VARCHAR,
     name VARCHAR
   );`);
@@ -23,21 +25,19 @@ const seed = async (data) => {
     article_id SERIAL PRIMARY KEY,
     title VARCHAR,
     body VARCHAR,
-    votes INT,
-    topic INT REFERENCES topics(slug),
-    author INT REFERENCES users(username),
-    created_at VARCHAR
+    votes INT DEFAULT 0,
+    topic VARCHAR REFERENCES topics(slug),
+    author VARCHAR REFERENCES users(username),
+    created_at DATE DEFAULT CURRENT_TIMESTAMP
   );`);
 	await db.query(`CREATE TABLE comments (
     comment_id SERIAL PRIMARY KEY,
-    author INT REFERENCES users(username),
+    author VARCHAR REFERENCES users(username),
     article_id INT REFERENCES articles(article_id),
-    votes INT,
-    created_at INT,
+    votes INT DEFAULT 0,
+    created_at DATE DEFAULT CURRENT_TIMESTAMP,
     body VARCHAR
   );`);
-	// 1. create tables
-
 	// 2. insert data
 };
 
