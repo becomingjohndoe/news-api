84246a4eb95413c65d221b71209e8bcdd8badc3a
becomingjohndoe <karl.swatman@gmail.com>
2021-11-01 16:11:30 +0000

    added patchArticleById function to article controllers

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,9 +1,21 @@
-const { selectArticleById } = require("../models/articles.model");
+const {
+	selectArticleById,
+	updateArticleById,
+} = require("../models/articles.model");
 
 exports.getArticlesById = async (req, res, next) => {
 	try {
 		const article = await selectArticleById(req.params);
 		res.status(200).send({ article });
+	} catch (err) {
+		next(err);
+	}
+};
+
+exports.patchArticleById = async (req, res, next) => {
+	try {
+		const article = await updateArticleById(req.params);
+		res.status(201).send({ article });
 	} catch (err) {
 		next(err);
 	}
