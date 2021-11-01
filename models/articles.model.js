ccc078e3028c6da5b0e4a13774f2c9937c921d3f
becomingjohndoe <karl.swatman@gmail.com>
2021-11-01 17:05:18 +0000

    added comment count to /api/articles/:article_id

--- models/articles.model.js
+++ models/articles.model.js
@@ -2,7 +2,10 @@ const db = require("../db/connection");
 
 exports.selectArticleById = async ({ article_id }) => {
 	const { rows } = await db.query(
-		"SELECT * FROM articles WHERE article_id = $1;",
+		`SELECT articles.*, COUNT(comments.comment_id) AS comment_count FROM articles 
+    LEFT JOIN comments ON comments.article_id = articles.article_id
+    WHERE articles.article_id = $1
+    GROUP BY articles.article_id;`,
 		[article_id]
 	);
 	return rows[0];
