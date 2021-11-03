feef071f4dde013fc56ebcb9222c09aa95dfac9b
becomingjohndoe <karl.swatman@gmail.com>
2021-11-03 16:16:21 +0000

    setup controller and model for /api/users/:user_id

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,9 +1,17 @@
-const { selectAllUsers } = require("../models/users.model");
+const { selectAllUsers, selectUserById } = require("../models/users.model");
 
 exports.getAllUsers = async (req, res, next) => {
 	try {
 		const users = await selectAllUsers();
 		res.status(200).send({ users });
+	} catch (err) {
+		next(err);
+	}
+};
+
+exports.getUserById = async (req, res, next) => {
+	try {
+		const user = await selectUserById(req.params);
 	} catch (err) {
 		next(err);
 	}
--- models/users.model.js
+++ models/users.model.js
@@ -4,3 +4,10 @@ exports.selectAllUsers = async () => {
 	const { rows } = await db.query(`SELECT username FROM users;`);
 	return rows;
 };
+
+exports.selectUserById = async ({ user_id }) => {
+	const { rows } = await db.query(`SELECT * FROM users WHERE user_id = $1;`, [
+		user_id,
+	]);
+	return rows[0];
+};
