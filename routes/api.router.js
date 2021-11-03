eb9f2d40baacdb5181a30a9e42b6d3aba6ee3def
becomingjohndoe <karl.swatman@gmail.com>
2021-11-03 15:07:59 +0000

    adds user router for /api/users

--- routes/api.router.js
+++ routes/api.router.js
@@ -2,6 +2,7 @@ const { endpoints } = require("../endpoints");
 const articleRouter = require("./article.router");
 const commentRouter = require("./comments.router");
 const topicRouter = require("./topic.router");
+const userRouter = require("./users.router");
 
 const apiRouter = require("express").Router();
 apiRouter.route("/").get((req, res) => {
@@ -10,4 +11,5 @@ apiRouter.route("/").get((req, res) => {
 apiRouter.use("/topics", topicRouter);
 apiRouter.use("/articles", articleRouter);
 apiRouter.use("/comments", commentRouter);
+apiRouter.use("/api/users", userRouter);
 module.exports = apiRouter;
