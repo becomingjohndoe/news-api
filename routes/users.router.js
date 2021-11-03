1f0fd9a0ec365d1370c6d8e06a3823f880256999
becomingjohndoe <karl.swatman@gmail.com>
2021-11-03 16:15:53 +0000

    adds GET /api/users/:user_id to user router

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,7 +1,8 @@
 const { getAllUsers } = require("../controllers/users.controller");
+const { selectUserById } = require("../models/users.model");
 
 const userRouter = require("express").Router();
 
 userRouter.route("/").get(getAllUsers);
-
+userRouter.route("/:user_id").get(selectUserById);
 module.exports = userRouter;
