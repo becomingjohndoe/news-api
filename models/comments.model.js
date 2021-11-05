9d2806fd4baaa712fa17a3f27d4cfd153c8d2fa3
becomingjohndoe <karl.swatman@gmail.com>
2021-11-05 08:32:45 +0000

    updated selectCommentsByArticlrId

--- models/comments.model.js
+++ models/comments.model.js
@@ -1,5 +1,6 @@
 const db = require("../db/connection");
 const { isUser } = require("../utils/isUser");
+const { selectArticleById } = require("./articles.model");
 
 exports.selectCommentsByArticleId = async ({ article_id }) => {
 	const { rows } = await db.query(
@@ -8,12 +9,10 @@ exports.selectCommentsByArticleId = async ({ article_id }) => {
     WHERE article_id = $1;`,
 		[article_id]
 	);
-	if (rows.length === 0) {
-		return Promise.reject({
-			status: 404,
-			msg: `no comments found for article ID ${article_id}`,
-		});
-	}
+	// check if article_id exists in articles table
+	// will return Promise reject if no articles found
+	await selectArticleById({ article_id });
+
 	return rows;
 };
 
@@ -43,7 +42,6 @@ exports.deleteCommentByCommentId = async ({ comment_id }) => {
 		`DELETE FROM comments WHERE comment_id = $1 RETURNING*;`,
 		[comment_id]
 	);
-
 	if (rows.length === 0) {
 		return Promise.reject({
 			status: 404,
