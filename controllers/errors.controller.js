3abb37089127b2e71dbca921835670a58ff6c7d3
becomingjohndoe <karl.swatman@gmail.com>
2021-11-05 18:16:39 +0000

    error handling for PSQL

--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -1,4 +1,5 @@
 exports.handleCustoms = (err, req, res, next) => {
+	console.log(err);
 	if (err.status) {
 		res.status(err.status).send({ msg: err.msg });
 	} else next(err);
@@ -7,4 +8,5 @@ exports.handleCustoms = (err, req, res, next) => {
 exports.handlePSQLS = (err, req, res, next) => {
 	console.log(err);
 	if (err.code === "22P02") res.status(400).send({ msg: "Invalid input type" });
+	if (err.code === "23503") res.status(404).send({ msg: "Resource not found" });
 };
