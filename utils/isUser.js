8d3ef97b9a93782caa660c4975aafa34997365c3
becomingjohndoe <karl.swatman@gmail.com>
2021-11-05 11:23:16 +0000

    delete unnecessary files

--- utils/isUser.js
+++
@@ -1,8 +0,0 @@
-const db = require("../db/connection");
-
-exports.isUser = async (user) => {
-	const { rows } = await db.query(`SELECT * FROM users WHERE username = $1;`, [
-		user,
-	]);
-	return rows.length === 0 ? false : true;
-};
