abbb5558631f220732f7478299276d7e7f819df7
becomingjohndoe <karl.swatman@gmail.com>
2021-11-05 11:11:34 +0000

    adds error handling to selectUserByNa

--- models/users.model.js
+++ models/users.model.js
@@ -9,5 +9,8 @@ exports.selectUserByName = async ({ username }) => {
 	const { rows } = await db.query(`SELECT * FROM users WHERE username = $1;`, [
 		username,
 	]);
+	if (rows.length === 0) {
+		return Promise.reject({ status: 404, message: "User not found" });
+	}
 	return rows[0];
 };
